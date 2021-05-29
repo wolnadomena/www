1568cfbcb320bced80ee6dcba22ac20792866d90
tom <tom@sapletta.com>
2021-05-29 19:09:14 +0200

    cleaning

--- index.php
+++ index.php
@@ -258,7 +258,7 @@ try {
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Test Your domains list</title>
+    <title>Wolna Domena .pl - Check Your domains list</title>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
